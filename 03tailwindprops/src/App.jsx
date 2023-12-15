bf68586cb93d771872ba1bc597faf3460b012388
hafijur <hafijur0211@gmial.com>
2023-12-15 22:53:19 +0530

    tailwind and props in reactjs

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Card from "./components/Cards";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  let myObj = {
+    username: "hafijur",
+    age: 24,
+  };
+
+  let newArr = [1, 2, 3, 4];
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind test
+      </h1>
+      <Card username="chai aur code" btnText="Click me" />
+      <Card username="Hafijur" />
+    </>
+  );
+}
+
+export default App;
